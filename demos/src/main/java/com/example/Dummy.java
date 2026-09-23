174654eafa60d93f621a4354606b20476ac5e94f
Javier <35864946+jmagit@users.noreply.github.com>
2026-09-23 16:32:01 +0200

    solucionado IEEE754

--- demos/src/main/java/com/example/Dummy.java
+++ demos/src/main/java/com/example/Dummy.java
@@ -1,7 +1,18 @@
 package com.example;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Dummy {
 	public double suma(double a, double b) {
-		return a + b;
+		return roundIEEE754(a + b);
 	}
+	
+
+	private double roundIEEE754(double o) {
+		return BigDecimal.valueOf(o)
+				.setScale(16, RoundingMode.HALF_UP)
+				.doubleValue();
+	}
+
 }
--- demos/src/test/java/com/example/DummyTest.java
+++ demos/src/test/java/com/example/DummyTest.java
@@ -1,14 +1,17 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import com.example.Dummy;
-
+@DisplayName("Pruebas de la clase Dummy")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DummyTest {
 
 	@BeforeAll
@@ -24,21 +27,30 @@ class DummyTest {
 	}
 
 	@Test
-	void testSuma() {
+	void test_Suma_dos_enteros() {
 		var c = new Dummy();
 		
 		var actual = c.suma(1, 2);
 		
 		assertEquals(3,  actual);
 	}
+	
+	@Test
+	void test_Suma_dos_decimales() {
+		var c = new Dummy();
+		
+		assertEquals(3.0, c.suma(2.0, 1.0));
+	}
 
 	@Test
+	@DisplayName("Valida el error de precisión IEEE754")
 	void testSumaIEEE() {
 		var c = new Dummy();
 		
 		var actual = c.suma(0.1, 0.2);
 		
 		assertEquals(0.3,  actual);
+		assertEquals(0.1,  c.suma(1, -0.9));
 	}
 
 }
