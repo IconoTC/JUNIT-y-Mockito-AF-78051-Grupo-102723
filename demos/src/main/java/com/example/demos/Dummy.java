c2182d0db263bc230707d3cfaf351e77c550c7a4
Javier <35864946+jmagit@users.noreply.github.com>
2026-09-24 15:33:11 +0200

    refactor

--- demos/src/main/java/com/example/demos/Dummy.java
+++ demos/src/main/java/com/example/demos/Dummy.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.demos;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
--- demos/src/test/java/com/example/demos/DummyTest.java
+++ demos/src/test/java/com/example/demos/DummyTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.demos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
